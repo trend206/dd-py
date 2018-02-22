cca8ea0ac95ab29e8463aba6714005c7dd16c15e
Jeff Thorne <jeff.thorne@gmail.com>
2018-02-22 16:32:36 -0500

    first commit

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='dd-py',
-      version='0.1a',
+      version='0.1b',
       description='Python 3 client for Trend Micro\'s Deep Discovery Platform',
       url='https://github.com/trend206/ddpy',
       author='Jeff Thorne',
       author_email='jthorne@u.washington.edu',
       license='MIT',
-      packages=['ddpy', 'ddpy.ddan', 'ddpy.utils'],
+      packages=['ddpy', 'ddpy.interfaces', 'ddpy.utils'],
       install_requires=['requests >= 2.9.1'],
       zip_safe=False)
